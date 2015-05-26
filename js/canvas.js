924e1e2172f70dd5a5d3e0d960a5e9a146e509b0
iamtheshazbot <jasonkhalili@gmail.com>
2015-05-25 21:12:56 -0700

    minor visual changes

--- js/canvas.js
+++ js/canvas.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 var value = 0; //Enter the amount of skins
 
-ctx.translate(0, 600); //Set origin to bottom right
+/*ctx.translate(0, 600); //Set origin to bottom right
 
 ctx.fillStyle="#E9EAED";
 ctx.fillRect(0,0, 200, -600);
@@ -13,7 +13,7 @@ ctx.fillRect(0,0, 200, -600);
 var skinstext = document.getElementById("numSkins");
 function fillBar() {
     ctx.fillStyle="#14a1ff";
-    ctx.fillRect(0, 0, 200, value * -2); //Up to 10 skins
+    ctx.fillRect(0, 0, value * 10, 200); //Up to 10 skins
     if(i % 30 == 0)
         skinstext.innerHTML = (i/30).toString() + " skins deposited";
     if(i <= skins*30) {
@@ -25,4 +25,4 @@ function fillBar() {
 };
 var i = 0;
 var skins = 10;
-var interval = window.setInterval(fillBar, 10);
+var interval = window.setInterval(fillBar, 20);
